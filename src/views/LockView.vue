042f983a9ade01d4a541ea25f42360e8ce521222
ElaisTheFixer <106221783+ElaisTheFixer@users.noreply.github.com>
2022-11-14 12:40:19 +1000

    Add files via upload

---
+++ src/views/LockView.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import { ref } from 'vue'
+    import LockStep1 from '../components/LockStep1.vue'
+    import LockStep2 from '../components/LockStep2.vue'
+
+    const step1 = ref(true)
+    const step2 = ref(false)
+</script>
+
+<template>
+     <LockStep1 :show="step1" @continue="step1 = false; step2 = true;" />
+     <LockStep2 :show="step2" />
+</template>
